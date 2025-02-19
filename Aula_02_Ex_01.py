960d4eb0f53d3b4c0cf9ef139fbc7f9e2c46a42b
Lucas-de-Sa-08 <lucaslopesdesa8@gmail.com>
2025-02-19 15:30:28 -0300

    .

---
+++ Aula_02_Ex_01.py
@@ -0,0 +1,3 @@
+Altura = float(input("Informe sua altura:"))
+Peso_ideal = (72.7*Altura-58)
+print("Seu Peso ideal é:",Peso_ideal)
---
+++ Aula_02_Teste_01.py
@@ -0,0 +1,4 @@
+anoNasc = int(input("Informe o ano de nascimento:"))
+anoAtual = int(input("Informe o ano atual:"))
+idade = anoAtual - anoNasc
+print("Sua idade é:",idade)
