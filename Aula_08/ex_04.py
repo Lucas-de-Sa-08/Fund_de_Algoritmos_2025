3eeb45787f3c86b502a44757721d201fb14c29c1
Lucas Lopes de Sá <lucaslopesdesa8@gmail.com>
2025-03-30 21:51:15 -0300

    .

--- Aula_08/ex_04.py
+++ Aula_08/ex_04.py
@@ -5,20 +5,18 @@ for i in range(10):
     numeros.append(num)
 
 soma_pares = 0
-for num in numeros
+for num in numeros:
+    if num % 2 == 0:
+        soma_pares += num
+
+soma_indice_par = 0
+for i in range(0, 10, 2):
+    soma_indice_par += numeros[i]
 
 print("\nLista: ", numeros)
+print("\nSoma dos elementos pares: ", soma_pares)
+print("\nSoma dos índices pares: ", soma_indice_par)
 
-#T = [11, 7, 2, 4]
-#menor = 999999
-
-#for i in range(len(T)):
-#    if T[i] < menor:
-#        menor = T[i]
-
-#print(T)
-#print("Menor valor: ", menor)
-######################################################################
 # Criar uma lista com 10 elementos fornecidos pelo usuário
 #numeros = []
 #for i in range(10):
@@ -26,7 +24,7 @@ print("\nLista: ", numeros)
 #    numeros.append(num)
 
 # Calcular a soma dos elementos pares
-soma_pares = sum(num for num in numeros if num % 2 == 0)
+#soma_pares = sum(num for num in numeros if num % 2 == 0)
 
 # Calcular a soma dos elementos nos índices pares
 #soma_indices_pares = sum(numeros[i] for i in range(0, 10, 2))
@@ -34,4 +32,3 @@ soma_pares = sum(num for num in numeros if num % 2 == 0)
 # Exibir os resultados
 #print(f"Soma dos elementos pares: {soma_pares}")
 #print(f"Soma dos elementos nos índices pares: {soma_indices_pares}")
-######################################################################
